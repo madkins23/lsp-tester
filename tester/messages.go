dbf78c64e521a246fea91a4e770ec805b5ffdeeb
mAdkins <software@doorways.org>
2023-05-11 09:11:40 -0700

    Replaced simple message format code with one that recognizes types of some messages

--- tester/messages.go
+++ tester/messages.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"math/rand"
 	"net"
@@ -14,29 +15,39 @@ import (
 	"github.com/rs/zerolog"
 )
 
+type genericData map[string]any
+
+func (gd genericData) hasField(name string) bool {
+	_, found := gd[name]
+	return found
+}
+
 type message struct {
 	JSONRPC string `json:"jsonrpc"`
 }
 
+// request represents the
+// RequestMessage, NotificationMessage, Registration, and Unregistration messages.
 type request struct {
 	message
-	ID     int         `json:"id"`
-	Method string      `json:"method"`
-	Params interface{} `json:"params"`
+	ID              int    `json:"id"`
+	Method          string `json:"method"`
+	Params          any    `json:"params"`
+	RegisterOptions any    `json:"registerOptions"`
 }
 
-//type responseError struct {
-//	Code    int         `json:"code"`
-//	Message string      `json:"message"`
-//	Data    interface{} `json:"data"`
-//}
+type responseError struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+	Data    any    `json:"data"`
+}
 
-//type response struct {
-//	message
-//	ID     int           `json:"id"`
-//	Result interface{}   `json:"result"`
-//	Error  responseError `json:"error"`
-//}
+type response struct {
+	message
+	ID     int            `json:"id"`
+	Result any            `json:"result"`
+	Error  *responseError `json:"error"`
+}
 
 func formatMessageJSON(m map[string]interface{}, buffer *bytes.Buffer) error {
 	if msg, found := m["msg"]; found {
@@ -91,7 +102,7 @@ func sendRequest(to string, rqst *request, connection net.Conn) error {
 	rqst.JSONRPC = jsonRpcVersion
 	rqst.ID = rand.Intn(idRandomRange)
 
-	if params, ok := rqst.Params.(map[string]interface{}); ok {
+	if params, ok := rqst.Params.(genericData); ok {
 		if path, found := params["path"]; found {
 			if relPath, ok := path.(string); ok {
 				if absPath, err := filepath.Abs(relPath); err == nil {
@@ -121,7 +132,7 @@ func sendContent(to string, content []byte, connection net.Conn) error {
 
 var (
 	methodByID = make(map[int]string)
-	paramByID  = make(map[int]string)
+	paramsByID = make(map[int]any)
 )
 
 func logMessage(from, to, msg string, content []byte) {
@@ -147,27 +158,15 @@ func logMessage(from, to, msg string, content []byte) {
 	event := log.Info().Str("!", direction).Int("#size", len(content))
 
 	if simpleFormat {
-		data := make(map[string]interface{})
+		data := make(genericData)
 		if err := json.Unmarshal(content, &data); err != nil {
 			log.Warn().Err(err).Msg("Unmarshal content")
 			// Fall through to end where raw JSON is added.
 		} else {
-			var ID int
-			if id, found := data["id"]; found {
-				if number, ok := id.(float64); ok {
-					ID = int(number)
-					event.Int("$ID", ID)
-				}
+			event = log.Info().Str("!", direction).Int("#size", len(content))
+			if err := simpleMessageFormat(data, content, event, msg); err != nil {
+				log.Warn().Err(err).Msg("simpleMessageFormat()")
 			}
-
-			// TODO: Is there a good way to find a field exists and
-			// then unmarshal the data into a specific struct for better output?
-			// https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification
-			addSomething("method", data, event, methodByID, ID, "from-method")
-			addSomething("params", data, event, paramByID, ID, "method-params")
-			addSomething("result", data, event, nil, ID, "")
-			addSomething("error", data, event, nil, ID, "")
-			event.Msg(msg)
 			return
 		}
 	}
@@ -180,50 +179,123 @@ const (
 	maxHashDisplayLen   = 64
 )
 
-func addSomething(
-	key string, item interface{}, event *zerolog.Event,
-	remember map[int]string, id int, notFoundKey string,
-) bool {
-	var foundSomething bool
+var errUnknownMessageType = errors.New("Unknown Message Type")
+
+func simpleMessageFormat(data genericData, content []byte, event *zerolog.Event, msg string) error {
+	msgType := "unknown"
+	if data.hasField("method") {
+		var rqst request
+		if err := json.Unmarshal(content, &rqst); err != nil {
+			return fmt.Errorf("unmarshal request: %w", err)
+		}
+		msgType = "notification"
+		if rqst.ID > 0 {
+			msgType = "request"
+			event.Int("%ID", rqst.ID)
+		}
+		if rqst.Method != "" {
+			event.Str("%method", rqst.Method)
+			methodByID[rqst.ID] = rqst.Method
+		}
+		if rqst.Params != nil {
+			addToEventWithLog("params", rqst.Params, event)
+			paramsByID[rqst.ID] = rqst.Params
+		}
+		if rqst.RegisterOptions != nil {
+			msgType = "registration"
+			addToEventWithLog("options", rqst.RegisterOptions, event)
+		}
+	} else if data.hasField("result") {
+		var resp response
+		msgType = "response"
+		if err := json.Unmarshal(content, &resp); err != nil {
+			return fmt.Errorf("unmarshal response: %w", err)
+		}
+		if resp.ID > 0 {
+			event.Int("%ID", resp.ID)
+			if method, found := methodByID[resp.ID]; found {
+				event.Str("%rqst-method", method)
+			}
+			if params, found := paramsByID[resp.ID]; found {
+				addToEventWithLog("%rqst-params", params, event)
+			}
+		}
+		if resp.Error != nil {
+			if resp.Error.Code > 0 {
+				addToEventWithLog("error-code", resp.Error.Code, event)
+			}
+			if resp.Error.Message != "" {
+				addToEventWithLog("error-msg", resp.Error.Message, event)
+			}
+			if resp.Error.Data != nil {
+				addToEventWithLog("error-data", resp.Error.Data, event)
+			}
+		}
+		addToEventWithLog("result", resp.Result, event)
+	}
+	if msgType == "unknown" {
+		event.Err(errUnknownMessageType)
+		if str, err := marshalAny(data); err != nil {
+			return fmt.Errorf("marshalAny: %w", err)
+		} else {
+			event.Str("msg", str)
+		}
+	} else {
+		event.Str("$Type", msgType)
+	}
+	event.Msg(msg)
+	return nil
+}
+
+func addToEventWithLog(key string, item any, event *zerolog.Event) {
+	if found, err := addToEvent(key, item, event); err != nil {
+		log.Warn().Err(err).Msgf("Adding %s to event", key)
+	} else if !found {
+		log.Debug().Msgf("Empty %s", key)
+	}
+}
+
+func addToEvent(key string, item any, event *zerolog.Event) (bool, error) {
+	added := true
 	if text, ok := item.(string); ok {
 		if len(text) > maxStringDisplayLen {
 			text = text[:maxStringDisplayLen]
 		}
 		event.Str(key, text)
-		if remember != nil {
-			remember[id] = text
-		}
-		foundSomething = true
 	} else if number, ok := item.(float64); ok {
 		event.Float64(key, number)
-		foundSomething = true
 	} else if boolean, ok := item.(bool); ok {
 		event.Bool(key, boolean)
-		foundSomething = true
 	} else if hash, ok := item.(map[string]interface{}); ok && len(hash) > 0 {
 		for _, attempt := range []string{key, "text", "path", "value", "data"} {
 			if something, found := hash[attempt]; found {
-				foundSomething = addSomething(key, something, event, remember, id, notFoundKey)
-				if foundSomething {
-					break
-				} else if someHash, ok := something.(map[string]interface{}); ok && len(someHash) > 0 {
-					if resultBytes, err := json.Marshal(someHash); err != nil {
-						log.Warn().Err(err).Msg("Marshal result")
-					} else {
-						if len(resultBytes) > maxHashDisplayLen {
-							resultBytes = append(resultBytes[:maxHashDisplayLen], []byte("...")...)
-						}
-						event.Bytes(key, resultBytes)
-						foundSomething = true
-					}
+				if done, err := addToEvent(key, something, event); err != nil {
+					return false, fmt.Errorf("addToEvent: %w", err)
+				} else if done {
+					return true, nil
 				}
 			}
 		}
+		added = false
 	}
-	if id > 0 && !foundSomething {
-		if something, found := remember[id]; found {
-			event.Str(notFoundKey, something)
+	if !added {
+		if str, err := marshalAny(item); err != nil {
+			return false, fmt.Errorf("marshalAny: %w", err)
+		} else {
+			event.Str(key, str)
+			added = true
 		}
 	}
-	return foundSomething
+	return true, nil
+}
+
+func marshalAny(item any) (string, error) {
+	if jsonBytes, err := json.Marshal(item); err != nil {
+		return "", fmt.Errorf("marshal JSON: %w", err)
+	} else {
+		if len(jsonBytes) > maxHashDisplayLen {
+			jsonBytes = append(jsonBytes[:maxHashDisplayLen], []byte("...")...)
+		}
+		return string(jsonBytes), nil
+	}
 }
